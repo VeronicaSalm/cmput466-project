eaebd296e60f62324661cea5c108edbc97799589
Dale Yue <cadaleyu@gmail.com>
2021-02-17 21:33:31 +0800

    Satchel created class for managing input data

--- main.py
+++ main.py
@@ -12,6 +12,34 @@ import nltk
 # nltk.download('punkt')
 # nltk.download('wordnet')
 
+class management:
+
+    def __init__(self):
+
+        self.__all_data = []
+        self.__path = None
+        self.__classified_data = {}
+        self.__data_class = []
+        self.__class_space = []
+
+    def loadIn(self, path):
+        self.__path = path
+        self.__class_space = os.listdir(path)
+        for each in self.__class_space:
+            self.__classified_data[each] = []
+            file_path = os.path.join(path, each)
+
+            for f in os.listdir(file_path):
+                name = os.path.join(file_path, f)
+                opening = open(name, "rb")
+                temp = tokenize(opening.read().decode(
+                    "utf-8", errors="ignore"))
+                temp = normalize(temp)
+
+                self.__all_data.append(temp)
+                self.__classified_data[each].append(temp)
+                self.__data_class.append(each)
+
 
 def tokenize(text, rmStopWords=False):
     # tokenize a text, remove punctuations
