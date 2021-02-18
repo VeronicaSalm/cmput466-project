8bc6ed23bb4f139f37897c1ee363b315b719123a
xiang zhang <xzhang23@ualberta.ca>
2021-02-17 22:27:16 -0700

    changed nothing but to reverse back to original commit before merge

--- newsgroup.py
+++
@@ -1,6 +0,0 @@
-from settings import *
-from sklearn.datasets import fetch_20newsgroups
-
-newsgroups_train = fetch_20newsgroups(subset='train')
-
-print(newsgroups_train)
--- settings.py
+++
@@ -1,9 +0,0 @@
-# Project wide settings and constants.
-
-
-# Debug mode. If true, more verbose output will be printed.
-DEBUG = True
-
-# File path to store the 20newsgroup dataset.
-# It is small enough that it can be stored locally.
-NEWS_PATH='./'
