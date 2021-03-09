84f78b69e01c11c86e52b3d1adfa39db27bcded0
Veronica <vsalm@ualberta.ca>
2021-03-09 16:37:50 -0700

    removed examples

--- example_tweets/pretty.py
+++
@@ -1,7 +0,0 @@
-import json
-
-with open("tweet.json", "r") as fobj:
-    parsed = json.loads(fobj.read())
-
-with open("tweet.json", "w") as fobj:
-    print(json.dumps(parsed, indent=4), file=fobj)
