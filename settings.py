b0261d5fc1379858f2c7ab86aebc0cf6cae747c0
Noah Gergel <n.gergel1@gmail.com>
2021-03-10 15:03:55 -0700

    Updated settings.py to have twitter file paths

--- settings.py
+++ settings.py
@@ -15,3 +15,7 @@ TWITTER_DIR = os.path.join(PROJECT_DIR, 'twitter-data')
 NEWSGROUP_TRAIN = os.path.join(NEWSGROUP_DIR, 'newsgroup_train.tsv')
 NEWSGROUP_TEST = os.path.join(NEWSGROUP_DIR, 'newsgroup_test.tsv')
 NEWSGROUP_CLASSES = os.path.join(NEWSGROUP_DIR, 'newsgroup_classes.tsv')
+
+TWITTER_TRAIN = os.path.join(TWITTER_DIR, 'twitter_train.tsv')
+TWITTER_TEST = os.path.join(TWITTER_DIR, 'twitter_test.tsv')
+TWITTER_CLASSES = os.path.join(TWITTER_DIR, 'twitter_classes.tsv')
