dacd6790689440ea6d96f60ae67c4840527e5493
Noah Gergel <n.gergel1@gmail.com>
2021-03-11 13:30:46 -0700

    updated to properly get topics

--- NMF.py
+++ NMF.py
@@ -9,7 +9,71 @@ from sklearn.datasets import fetch_20newsgroups
 from DataManager import DataManager
 import settings
 
-n_feat = 1000
+def get_confusion_matrix(generated_topics, real_topics):
+    '''
+    Gives a confusion matrix for a set of generated topics. 
+    Generated based on assigning real topics using the assign_topics function.
+    Arguments:
+        - generated_topics (list): The generated topics for each document. 
+        generated_topics[i] is the generated topic of the i'th document in the corpus.
+        - real_topics (list): The real topics for each document, as labelled in the dataset.
+        real_topics[i] is the real topic of the i'th document in the corpus.
+        The dimensions of generated_topics and real_topics should match.
+    
+    Returns:
+        The confusion matrix out, represented as a dict mapping tuples to counts.
+        out[(topicA, topicB)] is the number of documents predicted as topicA that are actually topicB
+    '''
+    assigned_topics = assign_topics(generated_topics, real_topics)
+
+    out = dict()
+    for real_topic in set(real_topics):
+        for real_topic2 in set(real_topics):
+            out[(real_topic, real_topic2)] = 0
+
+    for generated_topic, real_topic in zip(generated_topics, real_topics):
+        out[(assigned_topics[generated_topic], real_topic)] += 1
+    
+    return out
+
+
+def assign_topics(generated_topics, real_topics):
+    '''
+    Assign real names in a "heuristic" manner to auto generated topics.
+    For each generated topic, assign it to the real topic that is most often assigned to it, throwing out real topics that we have aalready assigned. 
+    Arguments:
+        - generated_topics (list): The generated topics for each document. 
+        generated_topics[i] is the generated topic of the i'th document in the corpus.
+        - real_topics (list): The real topics for each document, as labelled in the dataset.
+        real_topics[i] is the real topic of the i'th document in the corpus.
+        The dimensions of generated_topics and real_topics should match.
+    
+    Returns:
+        A dict mapping values in generated_topics to values in real_topics.
+    '''
+    out = dict()
+    used_real_topics = set()
+    for topic in set(generated_topics):
+        real_topic_count = dict()
+        for real_topic in set(real_topics):
+            real_topic_count[real_topic] = 0
+        for generated_topic, real_topic in zip(generated_topics, real_topics):
+            if generated_topic == topic:
+                real_topic_count[real_topic] += 1
+        
+        # sort in descending order by value
+        sorted_counts = sorted(real_topic_count.items(), key = lambda x: -x[1])
+        
+        for real_topic, count in sorted_counts:
+            if real_topic not in used_real_topics:
+                # if we haven't used this topic, then use it and mark it so we don't use it again
+                out[topic] = real_topic 
+                used_real_topics.add(real_topic)
+                break
+        
+    return out
+
+n_feat = 20000
 
 dm = DataManager(settings.NEWSGROUP_DIR, 'newsgroup')
 dm.load_data()
@@ -22,21 +86,56 @@ tfidf_vectorizer = TfidfVectorizer(max_df=0.95, min_df=2,
                                    stop_words='english')
 tfidf = tfidf_vectorizer.fit_transform(train_data)
 
-nmf = NMF(n_components=20, random_state=1, alpha=.1, l1_ratio=.5).fit(tfidf)
+nmf = NMF(n_components=20, random_state=1,
+          beta_loss='kullback-leibler', solver='mu', max_iter=1000, alpha=0.1,
+          l1_ratio=0.5).fit(tfidf)
 
 tfidf_new = tfidf_vectorizer.transform(test_data)
-X_new = nmf.transform(tfidf_new)
-predicted = [np.argsort(doc)[::-1][0] for doc in X_new]
+predicted = nmf.transform(tfidf_new)
 
-cnt = len(test_data)
-correct = 0
+#print(get_confusion_matrix(predicted, dm.get_data_classes(True)))
 
-labels = dm.get_classes()
+dominant_topic = np.argmax(predicted, axis=1)
+#df_document_topic['dominant_topic'] = dominant_topic
 
-for i in range(len(test_data)):
-    if labels[predicted[i]] == dm.get_label(i):
-        correct += 1
-    else:
-        print('predicted:', labels[predicted[i]], 'true:', dm.get_label(i))
+print(dominant_topic)
 
-print(correct / cnt)
+real_topics = [x[0] for x in dm.get_all_data(True)]
+
+print(real_topics)
+
+conf_mat = get_confusion_matrix(dominant_topic, real_topics)
+
+acc_num = 0
+acc_denom = 0
+for key in conf_mat:
+    acc_denom += conf_mat[key]
+    if key[0] == key[1]:
+        acc_num += conf_mat[key]
+print("accuracy =", acc_num/acc_denom)
+
+total_prec = 0
+total_recall = 0
+
+for real_topic in set(real_topics):
+    numerator = 0
+    prec_denom = 0
+    recall_denom = 0
+    for key in conf_mat:
+        if key[0] == real_topic == key[1]:
+            numerator += conf_mat[key]
+            prec_denom += conf_mat[key]
+            recall_denom += conf_mat[key]
+        elif key[0] == real_topic:
+            prec_denom += conf_mat[key]
+        elif key[1] == real_topic:
+            recall_denom += conf_mat[key]
+    if prec_denom != 0:
+        print(real_topic, "precision =", numerator/prec_denom)
+        total_prec += numerator/prec_denom
+    if recall_denom != 0:
+        print(real_topic, "recall =", numerator/recall_denom)
+        total_recall += numerator/recall_denom
+
+print("avg precision =", total_prec/20)
+print("avg recall =", total_recall/20)
