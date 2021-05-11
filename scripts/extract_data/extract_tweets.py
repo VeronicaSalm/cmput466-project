17ddcf776dfce6b09b5ce49a736fe6f70201b327
Veronica Salm <vsalm@ualberta.ca>
2021-05-11 20:17:21 +0000

    Updating extraction scripts

--- scripts/extract_data/extract_tweets.py
+++ scripts/extract_data/extract_tweets.py
@@ -11,6 +11,23 @@ import os
 import sys
 import csv
 import nltk, string
+import spacy
+import kenlm
+from collections import defaultdict
+from hashtag_master.word_breaker.main import segment_word
+from hashtag_master.neural_ranker.main import create_neural_ranking_model
+from hashtag_master.neural_ranker.rerank import rerank
+from textblob import TextBlob
+
+# For Hashtag Master
+model_type = "mse_multi"
+print("Loading language model.")
+language_model = kenlm.LanguageModel("hashtag_master/data/small_gt.bin")
+print("Done.")
+
+print("Training Neural Ranking Model.")
+neural_ranking_model, feature_extractor = create_neural_ranking_model(model_type)
+print("Done")
 
 # Alternate language identification methods:
 # TextBlob works, but unfortunately the scale of our project exceeds API limits
@@ -48,16 +65,68 @@ URLS = "urls"
 REPLY = "in_reply_to_user_id"
 ID = "id"
 
+def get_features(candidates):
+    """
+    Extracts the feature vector for the candidate segmentations.
+    """
+    global feature_extractor
+    best_cand = candidates[0]
+    feats = []
+    for seg in candidates:
+        fv = feature_extractor._get_features_for_segmentation(seg, best_cand)
+        feats.append(fv)
+    return feats
+
+def segment(t):
+    """
+    Attempts to segment the hashtag represented by "t" using the HashtagMaster tool.
+    """
+    if t.startswith("#") and len(t) > 1:
+        global language_model
+        global neural_ranking_model
+        top_k = 5
+        candidates = segment_word(t.lstrip("#"), 5, language_model)
+        if len(candidates) > 1:
+            # there are multiple candidates, run the reranking algorithm
+            feats = get_features(candidates)
+            # print(feats)
+            reranked_segs = rerank(candidates, feats, neural_ranking_model, model_type)
+            best = reranked_segs[0].split()
+        elif len(candidates) == 1:
+            best = candidates[0].split()
+        else:
+            print(t, candidates)
+            raise Exception("Error, not enough candidates for hashtag '{}'!".format(t))
+
+        return best
+    else:
+        return t
+
+def tokenize(text):
+    " Removes URLs, segments hashtags, and produces a list of tokens for language detection."
+    text = text.replace(r'(https|http)?:\/(\w|\.|\/|\?|\=|\&|\%)*\b','')
+    text = text.replace(r'www\.\S+\.com','')
+    tokens = text.split()
+    
+    output = []
+    for t in tokens:
+        if t.startswith("#"):
+            output.extend(segment(t))
+        else:
+            output.append(t)
+
+    return output
+
 # initialize the set of english words
 with open("words_dictionary.json", "r") as fobj:
     english_words = json.loads(fobj.read())
 
-def detect_english(text, threshold = 0.70):
+def detect_english(tokens, threshold = 0.65):
     '''
     Determines whether or not an input text is in English.
 
     Arguments:
-        - text (string): the text to classify as English or Not English
+        - tokens (list): list of tokens
         - threshold (float): the percentage of words that must be English in order
                              for the text to be considered English (defaults to 70%)
 
@@ -66,17 +135,9 @@ def detect_english(text, threshold = 0.70):
     '''
     global english_words
 
-    words = text.split()
     normalized = []
-    for w in words:
+    for w in tokens:
         w = w.lower()
-
-        # ignore hashtags, which will rarely be a single English word
-        # hashtags like "#WearAMask" are very common, but the dictionary won't
-        # include them.
-        # The same is true of mentions
-        if w.startswith("#") or w.startswith("@"):
-            continue
 
         # remove punctuation
         w = w.translate(str.maketrans('', '', string.punctuation))
@@ -87,7 +148,6 @@ def detect_english(text, threshold = 0.70):
     E = 0
 
     # this case avoids division by 0 if all words are removed
-    # (e.g., because the tweet was entirely hashtags)
     if N == 0:
         return False
 
@@ -95,7 +155,8 @@ def detect_english(text, threshold = 0.70):
     for n in normalized:
         if n in english_words:
             E += 1
-
+    
+    print("Detect English:", E, N, E/N)
     # Return True if the number of English words is above the threshold
     if (E/N) > threshold:
         return True
@@ -121,16 +182,83 @@ def process_tweet_file(fpath, fdest):
     bad = 0
     dest_path = os.path.join(fdest, fpath.split('/')[-1])
     fobj = open(dest_path, "w")
-
+    
+    nlp = spacy.load("en_core_web_sm")
     with open(fpath, "r") as json_file:
         print("Processing '{}'...".format(fpath))
         line = json_file.readline()
 
+        cnt = 0
         while line:
             d = json.loads(line)
 
             tweet_ID = d[ID]
 
+            text = d[FULL_TEXT]
+            #text = "C'est une longue phrase avec de nombreux jetons français, que j'inclus pour voir ce que fait le segmenteur avec de longues phrases dans une autre langue"
+            # text = "Bueno cuando le dan la corona a Raven???"
+            doc = tokenize(text)
+            print(doc)
+            if detect_english(doc) or len(doc) < 10:
+                # this tweet is classified as English, so store it to the output
+                print("English!")
+                print(json.dumps(d), file=fobj)
+                cnt += 1
+            else:
+                bad += 1
+                print("Not English!")
+            """
+            if len(doc) < 10:
+                print(text)
+                lang = TextBlob(text)
+                if lang.detect_language() == "en":
+                    cnt += 1
+                    print("English!")
+                    print(json.dumps(d), file=fobj)
+                else:
+                    bad += 1
+                    print("Not English!")
+            else:
+                if detect_english(doc):
+                    # this tweet is classified as English, so store it to the output
+                    print("English!")
+                    print(json.dumps(d), file=fobj)
+                    cnt += 1
+                else:
+                    print(text)
+                    lang = TextBlob(text)
+                    if lang.detect_language() == "en":
+                        cnt += 1
+                        print("English!")
+                        print(json.dumps(d), file=fobj)
+                    else:
+                        bad += 1
+                        print("Not English!")
+            
+            """
+            """
+            doc = nlp(text)
+            print(doc)
+            propn = 0
+            total = 0
+            pos = defaultdict(int)
+            dep = defaultdict(int)
+            for token in doc:
+                print(token.text, token.pos_, token.dep_)
+
+                if token.pos_ == "PROPN":
+                    propn += 1
+                total += 1
+                pos[token.pos_] += 1
+                dep[token.dep_] += 1
+            print(d[FULL_TEXT])
+            print(propn, total, propn / total)
+            print(pos)
+            print(doc.has_annotation("DEP"))
+            print(dep)
+            """
+        
+            """
             # Check that this is not a retweet / reply and that it contains no
             # no media elements or urls.
             if d[REPLY] == None and len(d[ENTITIES][URLS]) == 0 \
@@ -150,7 +278,7 @@ def process_tweet_file(fpath, fdest):
             else:
                 # this is an invalid tweet
                 bad += 1
-
+            """ 
             # read the next line of file
             line = json_file.readline()
 
--- scripts/extract_data/extract_tweets.sh
+++ scripts/extract_data/extract_tweets.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
-python3 extract_tweets.py ../../../2020-01/ 2020-01-27-00 2020-02-02-23 ../../../TwitterDataset/data/Jan27-Feb02/
-python3 extract_tweets.py ../../../2020-02/ 2020-02-01-00 2020-02-02-23 ../../../TwitterDataset/data/Jan27-Feb02/
-python3 extract_tweets.py ../../../2020-03/ 2020-03-16-00 2020-03-22-23 ../../../TwitterDataset/data/Mar16-Mar22/
-python3 extract_tweets.py ../../../2020-04/ 2020-04-27-00 2020-05-03-23 ../../../TwitterDataset/data/Apr27-May03/
-python3 extract_tweets.py ../../../2020-05/ 2020-05-01-00 2020-05-03-23 ../../../TwitterDataset/data/Apr27-May03/
-python3 extract_tweets.py ../../../2020-07/ 2020-07-06-00 2020-07-12-23 ../../../TwitterDataset/data/Jul06-Jul12/
-
+python3 -m extract_tweets ../../../canadian_tweets_22k/2020-01/ 2020-01-01-00 2020-01-31-23 canadian_english/2020-01
+python3 -m extract_tweets ../../../canadian_tweets_22k/2020-02/ 2020-02-01-00 2020-02-31-23 canadian_english/2020-02
+python3 -m extract_tweets ../../../canadian_tweets_22k/2020-03/ 2020-03-01-00 2020-03-31-23 canadian_english/2020-03
+python3 -m extract_tweets ../../../canadian_tweets_22k/2020-04/ 2020-04-01-00 2020-04-31-23 canadian_english/2020-04
+python3 -m extract_tweets ../../../canadian_tweets_22k/2020-05/ 2020-05-01-00 2020-05-31-23 canadian_english/2020-05
+python3 -m extract_tweets ../../../canadian_tweets_22k/2020-06/ 2020-06-01-00 2020-06-31-23 canadian_english/2020-06
+python3 -m extract_tweets ../../../canadian_tweets_22k/2020-07/ 2020-07-01-00 2020-07-31-23 canadian_english/2020-07
+python3 -m extract_tweets ../../../canadian_tweets_22k/2020-08/ 2020-08-01-00 2020-08-31-23 canadian_english/2020-08
+python3 -m extract_tweets ../../../canadian_tweets_22k/2020-09/ 2020-09-01-00 2020-09-31-23 canadian_english/2020-09
+python3 -m extract_tweets ../../../canadian_tweets_22k/2020-10/ 2020-10-01-00 2020-10-31-23 canadian_english/2020-10
